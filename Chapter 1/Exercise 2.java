698f4e699bfe317fe8b59b78d4cc5ed17608868c
evan-poster <evan.poster@gmail.com>
2024-05-09 10:41:00 -0400

    Chapter 1 / Exercise 2 added

---
+++ Chapter 1/Exercise 2.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Ex 2: output the square of all numbers between 100 and 200 (inclusive)
+        for ( int i = 100; i <= 200; i++ ) {
+            double square = Math.sqrt(i);
+            System.out.println(square);
+        }
+    }
+}
