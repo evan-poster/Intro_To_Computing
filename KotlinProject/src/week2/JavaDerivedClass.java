fe6facd6535f988d09d0fc08cd2274acef2e762b
evan-poster <evan.poster@gmail.com>
2024-05-14 15:56:43 -0400

    Java Interface Example Added

---
+++ KotlinProject/src/week2/JavaDerivedClass.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class JavaDerivedClass implements JavaInterface {
+    @Override
+    public int function1() { return 1; }
+
+    @Override
+    public int function2() { return 0; }
+}
---
+++ KotlinProject/src/week2/JavaInterface.java
@@ -0,0 +1,9 @@
+package week2;
+
+// Not quite an abstract class, but allows abstract function declarations
+
+public interface JavaInterface {
+    public int function1();
+    public int function2();
+}
+
