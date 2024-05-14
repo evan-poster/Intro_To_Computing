5aa3f2086e010b6dab436866b2fbb9e82fb0b558
evan-poster <evan.poster@gmail.com>
2024-05-14 15:58:07 -0400

    java people class added

---
+++ KotlinProject/src/week2/People.java
@@ -0,0 +1,21 @@
+package week2;
+
+public class People {
+    private String name;
+    private int age;
+    private long SSN;
+    private String address;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public long getSSN() { return SSN; }
+    public void setSSN(long SSN) { this.SSN = SSN; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+}
