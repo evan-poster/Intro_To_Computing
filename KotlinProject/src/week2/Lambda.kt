2aad595fc659ca4517068527caff33febbdab339
evan-poster <evan.poster@gmail.com>
2024-05-14 15:57:16 -0400

    Kotlin Lambda added

--- KotlinProject/src/week2/Lambda.kt
+++ KotlinProject/src/week2/Lambda.kt
@@ -5,19 +5,20 @@ package week2
 fun main() {
     // Without lambdas
     val names = listOf("john", "jack", "jim")
-    var Names = mutableListOf<String>()
+    val Names = mutableListOf<String>()
     for (name in names) {
         Names.add(name[0].uppercase() + name.substring(1))
     }
-    println(Names)
+    println("Upper case names w/for loop: $Names")
 
      // With lambdas
     // { args : <code> }
     // map(){<code>}
+    // map {<code/lambda>}
     // can use 'it' as an implied keyword for simplicity. Will always reference object.
-    println(names.map({str -> str[0].uppercase() + str.substring(1)}))
-    println(names.map{it[0].uppercase() + it.substring(1)})
-    println(names.map(){it[0].uppercase() + it.substring(1)})
-    println(names.map(){it[0].uppercase().plus(it.substring(1))})
+    println(names.map { str -> str[0].uppercase() + str.substring(1) })
+    println(names.map() {it[0].uppercase() + it.substring(1)} )
+    println(names.map() {it[0].uppercase().plus(it.substring(1))} )
+    println(names.map {it[0].uppercase() + it.substring(1)} )
     println(names)
 }
