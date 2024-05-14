58e2755e484bcabe9247d010148d037561e765be
evan-poster <evan.poster@gmail.com>
2024-05-14 09:33:00 -0400

    Ch2/Ex2 Class Rename

--- Chapter 2/Exercise2.java
+++ Chapter 2/Exercise2.java
@@ -4,13 +4,13 @@
 // create max3 function such that it takes three numbers and return the largest one.
 
 
-public class Main {
+public class Exercise2 {
 
     public static void main(String[] args) {
-        System.out.println(newMax3(5, 6, 7));
+        System.out.println(max3(5, 6, 7));
     }
 
-    public static int newMax3(int a, int b, int c) {
+    public static int max3(int a, int b, int c) {
         // Nested ternary operators
         return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
     }
