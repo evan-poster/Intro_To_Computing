1e6edce471c577dd4752ed0761f229eab36fd81a
evan-poster <evan.poster@gmail.com>
2024-05-20 15:59:19 -0400

    CalculateE.java added

---
+++ KotlinProject/src/week2/CalculateE.java
@@ -0,0 +1,22 @@
+package week2;
+
+public class CalculateE {
+    public static double calculateE() {
+        double e = 1.0;
+        int n = 1;
+        double factorial = 1.0;
+
+        while (n <= 100) {
+            factorial *= n;
+            e += 1.0 / factorial;
+            n++;
+        }
+
+        return e;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateE());
+        System.out.println(Math.E);
+    }
+}
