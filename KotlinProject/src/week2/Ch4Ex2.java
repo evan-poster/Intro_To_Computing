43a2981740066562daafa3f03da174a0fd54b2db
evan-poster <evan.poster@gmail.com>
2024-05-21 20:15:39 -0400

    misc exercise files added (ch4)

---
+++ KotlinProject/src/week2/Ch4Ex2.java
@@ -0,0 +1,22 @@
+package week2;
+
+// Ex2: Reimplement the sorting algorithm listed on page 543 with Kotlin,
+//      and write some tests to verify your implementation.
+//
+//      Note: compile your code on command line first, then run it via command line.
+
+public class Ch4Ex2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void sort(String[] a)
+    {
+        int n = a.length;
+        for (int i = 1; i < n; i++)
+            for (int j = i; j > 0; j--)
+                if (a[j-1].compareTo(a[j]) > 0)
+                    System.out.println("foo");//exchange(a, j-1, j);
+                else break;
+    }
+}
